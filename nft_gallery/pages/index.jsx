0f579f68454f85d17372073779234866fd3ded10
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2023-01-16 15:37:36 -0500

    Update index.jsx

--- nft_gallery/pages/index.jsx
+++ nft_gallery/pages/index.jsx
@@ -10,8 +10,7 @@ const Home  = () => {
   const [collection, setCollectionAddress] = useState("")
   const [NFTs, setNFTs] = useState([])
   const [fetchForCollection, setFetchForCollection]=useState(false)
-  const [page,fetchPage] = useState("")
-  const [nextPage,fetchNextPage] = useState("")
+  const [page, fetchPage] = useState("")
   
   const fetchNFTs = async() => {
     let nfts; 
@@ -48,12 +47,12 @@ const Home  = () => {
       };
       const api_key = "<YOUR API KEY>"
       const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
-      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}&pageKey=${page}&pageSize=100`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}&startToken=${page}&limit=100`;
       const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
       if (nfts) {
         console.log("NFTs in collection:", nfts)
         setNFTs(nfts.nfts)
-        fetchPage(nfts.pageKey)
+        fetchPage(nfts.nextToken)
       }
     }
   }
@@ -62,15 +61,13 @@ const Home  = () => {
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
-        <input disabled={fetchForCollection} className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setWalletAddress(e.target.value); fetchPage("")}} value = {wallet} type={"text"} placeholder="Add your wallet address"></input>
-        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setCollectionAddress(e.target.value); fetchPage("")}} value={collection} type={"text"} placeholder="Add the collection address"></input>
-        <label className="text-gray-600 "><input onChange={(e)=>{; fetchPage("");setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
+        <input disabled={fetchForCollection} className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setWalletAddress(e.target.value)}} value = {wallet} type={"text"} placeholder="Add your wallet address"></input>
+        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-800 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} type={"text"} placeholder="Add the collection address"></input>
+        <label className="text-gray-600 "><input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection</label>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} 
           onClick={
-            () => {
-              //how do I get this thing to reset after it is set
-              fetchPage("") //does not work here but does work in the onChange events above
-              if (fetchForCollection) {
+            () => 
+              {if (fetchForCollection) {
                 fetchNFTsForCollection();
               }else 
                 fetchNFTs();  
@@ -88,7 +85,7 @@ const Home  = () => {
       </div>
       <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} 
-            disabled = {fetchPage.length}
+            disabled = {page.length == 0} 
             onClick={ 
               () => {
                 if (fetchForCollection) {
