0f4024334a3caf172c0b6466f3a76575027f543c
mtitus6 <84806551+mtitus6@users.noreply.github.com>
2023-02-09 20:59:25 -0500

    Add files via upload

---
+++ Dynamic NFT Chainlink/MockPriceFeed.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "@chainlink/contracts/src/v0.7/tests/MockV3Aggregator.sol";
+
+// code can be seen @ https://github.com/smartcontractkit/chainlink/tree/develop/contracts/src/v0.7/tests
+// pass decimals and initial price when deploying
+// deployed to 0xd9145CCE52D386f254917e481eB44e9943F39138
